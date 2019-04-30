3e8a4fd47878e85b5e21cd6863d0efa27bb1edd1
Volodymyr Vlasenko <vlastee@mail.ua>
2019-04-30 07:30:07 -0400

    updated
    bootstrap navigation added

--- functions.php
+++ functions.php
@@ -20,6 +20,12 @@ function include_jquery()
 }
 add_action('wp_enqueue_scripts', 'include_jquery');
 
+function load_bootstrap()
+{
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), '', 1, true);
+}
+add_action('wp_enqueue_scripts', 'load_bootstrap');
+
 function loadjs()
 {
 
@@ -31,6 +37,7 @@ function loadjs()
 add_action('wp_enqueue_scripts', 'loadjs');
 
 
+
 add_theme_support('menus');
 
 add_theme_support('post-thumbnails');
@@ -38,7 +45,18 @@ add_theme_support('post-thumbnails');
 
 register_nav_menus(
     array(
-        'top-menu' => __('Top Menu', 'theme'),
-        'footer-menu' => __('Footer Menu', 'theme'),
+        'primary' => __('Primary Menu', 'theme'),
+        'footer-menu' => __('footer-menu', 'theme'),
     )
 );
+
+
+
+// Register Custom Navigation Walker
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+	// file does not exist... return an error.
+	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+	// file exists... require it.
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
--- header.php
+++ header.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+$container = get_theme_mod( 'understrap_container_type' );
+?>
 <!DOCTYPE html>
 <html>
     
@@ -10,16 +23,79 @@
 <body <?php body_class();?>>
 
 
+<!-- ******************* The Navbar Area ******************* -->
+<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
-<header class="sticky-top">
+<a class="skip-link sr-only sr-only-focusable " href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+
+<nav class="navbar navbar-expand-md navbar-dark navbar-custom ">
+
+<?php if ( 'container' == $container ) : ?>
+	<div class="container " >
+<?php endif; ?>
+
+		
+
+		<button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<!-- The WordPress Menu goes here -->
+		<?php wp_nav_menu(
+			array(
+				'theme_location'  => 'primary',
+				'container_class' => 'collapse navbar-collapse mr-auto ',
+				'container_id'    => 'navbarNavDropdown',
+				'menu_class'      => 'navbar-nav mr-auto topnav ',
+				'fallback_cb'     => '',
+				'menu_id'         => 'main-menu',
+				'depth'           => 2,
+				'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+			)
+		); ?>
+	<?php if ( 'container' == $container ) : ?>
+	</div><!-- .container -->
+	<?php endif; ?>
+
+	<!-- Selected page lightup -->
+	
+
+
+	<!-- Your site title as branding in the menu -->
+	<?php if ( ! has_custom_logo() ) { ?>
+
+<?php if ( is_front_page() && is_home() ) : ?>
+
+	<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+<?php else : ?>
+
+	<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+<?php endif; ?>
+
+
+<?php } else {
+the_custom_logo();
+} ?><!-- end custom logo -->
+
+</nav><!-- .site-navigation -->
+
+
+
+<!--
+<header class="sticky-top"> 
         <div class="container">
 
 
-            <?php wp_nav_menu(
-                array(
-                    'theme_location' => 'top-menu',
-                    'menu_class' => 'navigation'
-                )
-            );?>
+            <?php $numbers = array(4, 6, 2, 22, 11);
+                sort($numbers);
+                
+               
+                 ?>
+                    <h1><span class="badge badge-secondary"><?php  print_r(array_reverse($numbers)); ?> </span></h1>
+                   
+            
         </div>
 </header>
+-->
